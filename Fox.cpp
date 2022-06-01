e14f97b8a1cdb7cfa58f2983f33c89df9b343049
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2022-06-01 09:03:24 +0300

    Herbivore finished

--- Fox.cpp
+++ Fox.cpp
@@ -1,6 +1,29 @@
 #include "Fox.h"
+#include "Herbivore.h"
 
 Animal* Fox::Interact(Animal& other)
 {
-	return nullptr;
+    Herbivore* pH = dynamic_cast<Herbivore*>(&other);
+    if (typeid(this).name() == typeid(other).name()) // Оба типа волк
+    {
+        int hp = rand() % 10;
+        int str = rand() % 4;
+        Fox temp(hp, str);
+        return &temp; // Порождается еще одна особь 
+    }
+    else
+        if (pH != nullptr) // Хищник и жертва 
+        {
+            int r = rand() % 100 + 1;
+            if (r < pH->luck)
+            {
+                this->health--;
+                return nullptr; // Жертва выживает 
+            }
+            else
+                pH->health = 0;
+            return nullptr; // Жертва умирает 
+        }
+        else
+            return nullptr; // Хищник и хищник
 }
--- Wolf.cpp
+++ Wolf.cpp
@@ -1,6 +1,29 @@
 #include "Wolf.h"
+#include "Herbivore.h"
 
 Animal* Wolf::Interact(Animal& other)
 {
-    return nullptr;
+    Herbivore* pH = dynamic_cast<Herbivore*>(&other);
+    if (typeid(this).name() == typeid(other).name()) // Оба типа волк
+    {
+        int hp = rand() % 10;
+        int str = rand() % 4;
+        Wolf temp(hp, str);
+        return &temp; // Порождается еще одна особь 
+    }
+    else
+        if (pH != nullptr) // Хищник и жертва 
+        {
+            int r = rand() % 100 + 1;
+            if (r < pH->luck)
+            {
+                this->health--;
+                return nullptr; // Жертва выживает 
+            }
+            else
+                pH->health = 0;
+            return nullptr; // Жертва умирает 
+        }
+        else
+            return nullptr; // Хищник и хищникreturn nullptr;
 }
